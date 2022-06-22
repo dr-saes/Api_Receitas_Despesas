e5931cf2b4aee00bfee25dcda75144b7aca0b118
Daniel Saes <dr_saes@icloud.com>
2022-06-22 15:10:49 -0300

    removendo o arquivo

--- app.js
+++
@@ -1,4 +0,0 @@
-console.log("Rodando aplicativo")
-console.log("Conectando com o banco de dados")
-console.log("Conectando com o repositorio local")
-console.log("Conectando com o repositorio do gitHub")
